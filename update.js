8aad8250db1aa53a828e8508430acdf4da6303f1
Fynn9563 <mobo9563@gmail.com>
2025-06-08 06:44:37 +1000

    Display download percent as a integer

--- update.js
+++ update.js
@@ -1,4 +1,3 @@
-// update.js
 const { BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
@@ -8,29 +7,40 @@ let updaterWindow = null;
 function initAutoUpdater(mainWin) {
   autoUpdater.on('update-available', () => {
     if (updaterWindow) return;
+
     updaterWindow = new BrowserWindow({
-      width: 400,
-      height: 160,
-      frame: true,
+      width: 320,
+      height: 200,
+      frame: false,
+      transparent: true,
       resizable: false,
       movable: true,
       alwaysOnTop: true,
       skipTaskbar: false,
+      autoHideMenuBar: true,
       show: false,
       webPreferences: {
         contextIsolation: false,
         nodeIntegration: true
       }
     });
+
+    updaterWindow.removeMenu();
     updaterWindow.loadFile(path.join(__dirname, 'update.html'));
     updaterWindow.once('ready-to-show', () => {
       updaterWindow.show();
     });
+
+    updaterWindow.on('closed', () => {
+      updaterWindow = null;
+    });
   });
 
   autoUpdater.on('download-progress', progress => {
+    // ensure percent is integer
+    const percentInt = Math.floor(progress.percent);
     updaterWindow?.webContents.send('download-progress', {
-      percent: Math.floor(progress.percent),
+      percent: percentInt,
       transferred: progress.transferred,
       total: progress.total
     });
